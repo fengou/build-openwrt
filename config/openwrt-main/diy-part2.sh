41ac2cb01aac56bb659c8cbfaf63789f8776098c
fengou <58597447+fengou@users.noreply.github.com>
2025-05-30 07:06:46 +0800

    Update diy-part2.sh
    Add luci-app-argon theme

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -25,6 +25,9 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 # Add luci-app-amlogic
 #svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
+#Add luci-app-argon theme
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lime-packages
+
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
 # svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
